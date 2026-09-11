bf7f475bf975d16a54eb66f05ff48f33b011f7ea
alfred1712 <alfred1712@gmail.com>
2026-09-11 10:03:56 -0300

    refactor: remove obsolete MediaProjection holder

--- app/src/main/java/com/example/lovale2/services/ScreenCaptureHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lovale2.services
-
-import android.content.Intent
-
-object ScreenCaptureHolder {
-    var resultCode: Int = 0
-    var resultData: Intent? = null
-    var isCapturing: Boolean = false
-}
